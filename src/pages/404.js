19251039c02b5661bf8cb4ecf97f19fa4b16f7fd
Andrea Mazzullo <mazzulloandrea@gmail.com>
2023-12-07 23:49:56 +0100

    clean 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,59 +1,10 @@
-import * as React from "react";
-import { routes, removeTrailingSlash } from "../utils";
-
-// import { Link } from "gatsby";
-
-// const pageStyles = {
-// 	color: "#232129",
-// 	padding: "96px",
-// 	fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// };
-// const headingStyles = {
-// 	marginTop: 0,
-// 	marginBottom: 64,
-// 	maxWidth: 320,
-// };
-
-// const paragraphStyles = {
-// 	marginBottom: 48,
-// };
-// const codeStyles = {
-// 	color: "#8A6534",
-// 	padding: 4,
-// 	backgroundColor: "#FFF4DB",
-// 	fontSize: "1.25rem",
-// 	borderRadius: 4,
-// };
+import { routes, removeTrailingSlash } from '../utils';
 
 const NotFoundPage = () => {
-	if (
-		!Object.values(routes).includes(
-			removeTrailingSlash(window.location.pathname)
-		)
-	) {
-		window.location.pathname = "/";
-		return;
-	}
-	// return (
-	// 	<main style={pageStyles}>
-	// 		<h1 style={headingStyles}>Page not found</h1>
-	// 		<p style={paragraphStyles}>
-	// 			Sorry 😔, we couldn’t find what you were looking for.
-	// 			<br />
-	// 			{process.env.NODE_ENV === "development" ? (
-	// 				<>
-	// 					<br />
-	// 					Try creating a page in <code style={codeStyles}>src/pages/</code>.
-	// 					<br />
-	// 				</>
-	// 			) : null}
-	// 			<br />
-	// 			<Link to="/">Go home</Link>.
-	// 		</p>
-	// 	</main>
-	// );
+  if (!Object.values(routes).includes(removeTrailingSlash(window.location.pathname))) {
+    window.location.pathname = '/';
+    return;
+  }
 };
 
 export default NotFoundPage;
-
-// export const Head = () => <title>Not found</title>;
