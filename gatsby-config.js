24ca756219133c9c7695e296a8ff078c4ad9a6ea
Andrea Mazzullo <mazzulloandrea@gmail.com>
2023-12-07 23:43:56 +0100

    add dev_ssr to try to fix vercel deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  flags: {
+    DEV_SSR: true,
+  },
   siteMetadata: {
     title: `poggio-gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
